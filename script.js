c872a04faa4fe6f70bab5703d61454c7b2f458f7
marcus seus bocós <araujo.marcus@escola.pr.gov.br>
2025-11-14 22:03:55 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Ao clicar em qualquer botão do menu, abrir a página selecionada
 document.querySelectorAll(".btn").forEach(btn => {
   btn.addEventListener("click", () => {
     const target = btn.getAttribute("data-target");
-    if (target) {
-      window.location.href = target; // Redireciona para a página
-    }
+    if (target) window.location.href = target;
   });
 });
